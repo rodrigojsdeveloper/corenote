a6ee60847462e8fadeb0d792e7024b8db0c796b5
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-25 18:09:50 -0300

    feat: create entity Note

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import "express-async-errors";
 import cors from "cors";
 import "dotenv/config";
 
---
+++ server/src/entities/note.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("notes")
+class Note {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  is_favorite: boolean;
+
+  @Column()
+  color: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export default Note;
