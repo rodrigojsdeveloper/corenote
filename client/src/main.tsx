3c2dc0ea9b4a7b5fe3c5a25a0ec0706a30071777
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-25 14:39:55 -0300

    style: configure global style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import GlobalStyle from "./styles/global.ts";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
+  </React.StrictMode>
+);
---
+++ client/src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+	* {
+		border: 0;
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		resize: none;
+		box-sizing: border-box;
+		scroll-behavior: smooth;
+		font-family: 'Inter', sans-serif;
+	}
+
+	:root {
+    --color-white: #FFFFFF;
+    --color-black: #00000040;
+
+    --gray-100: #F0F2F5;
+    --gray-200: #D9D9D9;
+    --gray-300: #50656E;
+    --gray-400: #455A64;
+    --gray-500: #333333;
+    --gray-600: #95959540;
+
+    --transparent: transparent;
+	}
+
+	body {
+		color: var(--gray-500);
+		background-color: var(--gray-100);
+	}
+
+	button, a {
+		cursor: pointer; 
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	::-webkit-scrollbar-track {
+		background-color: var(--transparent);
+	}
+
+	::-webkit-scrollbar-thumb {
+		border-radius: 0.5px;
+		background-color: var(--grey-500);
+	}
+`;
