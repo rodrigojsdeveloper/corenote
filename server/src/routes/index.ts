887047f546665fcc229b2cc2d9200e9f376b25cf
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-26 00:05:49 -0300

    docs: create backend documentation with swagger

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Express } from "express";
 
 import noteRoutes from "./note.routes";
+import termsRoutes from "./terms.routes";
+
+import SwaggerDocs from "../../docs/swagger.json";
+import SwaggerUi from "swagger-ui-express";
 
 const appRoutes = (app: Express, endPoint: string): void => {
   app.use(`/${endPoint}/notes`, noteRoutes());
+  app.use(`/${endPoint}/terms`, termsRoutes());
+  app.use(`/${endPoint}/docs`, SwaggerUi.serve, SwaggerUi.setup(SwaggerDocs));
 };
 
 export { appRoutes };
---
+++ server/src/routes/terms.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import TermsController from "../controllers/terms.controller";
+
+const routes = Router();
+
+const termsRoutes = () => {
+  routes.get("", new TermsController().term);
+
+  return routes;
+};
+
+export default termsRoutes;
